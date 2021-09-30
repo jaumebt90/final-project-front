18b60e264e49ac6abb3ba88bf877868e25bdcd6c
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-09-30 16:40:36 +0200

    margin text club

--- src/pages/Club.js
+++ src/pages/Club.js
@@ -16,15 +16,15 @@ function Club() {
   return (
     <div className="club">
       <h1>Bienvenidos al +++++++++++++</h1>
-
-      <p className="texto">
-        Su primera temporada en el 'antiguo' San Paolo, lastrada por una lesión
-        de hombro, no fue excelsa.
-        <br /> Aún así, marcó 10 goles en 1.866 minutos. Ahora, en cambio, no
-        hay quien le pare. 'Osi' suma seis goles -cuatro en Liga- y una
-        asistencia en seis partidos. Una racha que tratará de ampliar en la UEFA
-        Europa League frente al Spartak Moscú.
-      </p>
+      <div className="texto">
+        <p>
+          Su primera temporada en el 'antiguo' San Paolo, lastrada por una
+          lesión de hombro, no fue excelsa. Aún así, marcó 10 goles en 1.866
+          minutos. Ahora, en cambio, no hay quien le pare. 'Osi' suma seis goles
+          -cuatro en Liga- y una asistencia en seis partidos. Una racha que
+          tratará de ampliar en la UEFA Europa League frente al Spartak Moscú.
+        </p>
+      </div>
     </div>
   );
 }
