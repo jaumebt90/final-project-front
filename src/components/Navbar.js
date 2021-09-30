9d8adba4006021e6bd79c4ed64ab253538916862
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-09-30 12:53:27 +0200

    dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,48 +2,43 @@ import { Link } from "react-router-dom";
 import logo from "../assets/escudoRacing.jpg";
 import { useContext } from "react"; // <== IMPORT
 import { AuthContext } from "./../context/auth.context"; // <== IMPORT
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
-function Navbar() {
+function Barra() {
   // Subscribe to the AuthContext to gain access to
   // the values from AuthContext.Provider `value` prop
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <nav>
-      <Link to="/">
-        <img src={logo} />
-        <button>Home</button>
-      </Link>
-      <Link to="/club">
-        <button>Club</button>
-      </Link>
-      -
-      {isLoggedIn ? (
-        <>
-          <Link to="/jugadas">
-            <button>Jugadas</button>
-          </Link>
-
-          <Link to="/pretemporada">
-            <button>Pretemporada</button>
-          </Link>
-          <button onClick={logOutUser}>Logout</button>
-          <span>{user.name}</span>
-        </>
-      ) : (
-        <>
-          <Link to="/signup">
-            {" "}
-            <button>Signup</button>{" "}
-          </Link>
-          <Link to="/login">
-            {" "}
-            <button>Login</button>{" "}
-          </Link>
-        </>
-      )}
-    </nav>
+    <>
+      <Navbar expand="lg" className="navbar">
+        <Container>
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/club">Club</Nav.Link>
+              <NavDropdown title="Perfil" id="basic-nav-dropdown">
+                {isLoggedIn ? (
+                  <>
+                    <Nav.Link href="/profile">Profile</Nav.Link>
+                    <Nav.Link href="/logout">Logout</Nav.Link>
+                  </>
+                ) : (
+                  <>
+                    <Nav.Link href="/signup">Signup</Nav.Link>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                  </>
+                )}
+                {/* <NavDropdown.Divider />
+                <Nav.Button onClick={logOutUser}>Logout</Nav.Button>*/}
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
-
-export default Navbar;
+export default Barra;
--- src/pages/Club.js
+++ src/pages/Club.js
@@ -14,8 +14,16 @@ function Club() {
       });
   }, []);
   return (
-    <div>
-      <h1>Club</h1>
+    <div className="club">
+      <h1>Bienvenidos al +++++++++++++</h1>
+
+      <p>
+        Su primera temporada en el 'antiguo' San Paolo, lastrada por una lesión
+        de hombro, no fue excelsa. Aún así, marcó 10 goles en 1.866 minutos.
+        Ahora, en cambio, no hay quien le pare. 'Osi' suma seis goles -cuatro en
+        Liga- y una asistencia en seis partidos. Una racha que tratará de
+        ampliar en la UEFA Europa League frente al Spartak Moscú.
+      </p>
     </div>
   );
 }
