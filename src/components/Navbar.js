c08e352843cbf9b807c68c220616b574cc9d06b0
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-10-04 15:51:51 +0200

    beauty nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import logo from "../assets/escudoRacing.jpeg";
 import { useContext } from "react"; // <== IMPORT
 import { AuthContext } from "./../context/auth.context"; // <== IMPORT
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import("./Navbar.css");
 
 function Barra() {
   // Subscribe to the AuthContext to gain access to
@@ -12,82 +13,84 @@ function Barra() {
 
   return (
     <header className="header">
-      <Navbar expand="lg" className="navbar">
+      <Navbar expand="lg" className="navbar" id="nav">
         <Container>
           <Link to="/">
             <img className="logo" src={logo} />
           </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse>
-            <Nav className="left">
-              <Nav.Link
-                onClick={() => {
-                  history.push("/");
-                }}
-              >
-                Home
-              </Nav.Link>
-              <Nav.Link
-                onClick={() => {
-                  history.push("/club");
-                }}
-              >
-                Club
-              </Nav.Link>
+          <Navbar.Collapse className="row justify-content-between">
+            <Nav className="col-3">
+              <div className="left">
+                <Nav.Link
+                  onClick={() => {
+                    history.push("/");
+                  }}
+                >
+                  Home
+                </Nav.Link>
+                <Nav.Link
+                  onClick={() => {
+                    history.push("/club");
+                  }}
+                >
+                  Club
+                </Nav.Link>
+              </div>
             </Nav>
-            <Nav className='right'>
-              <NavDropdown
-                title="Perfil"
-                id="basic-nav-dropdown"
-                className="desplegable"
-              >
-                {isLoggedIn ? (
-                  <>
-                    <Nav.Link
-                      onClick={() => {
-                        history.push("/profile");
-                      }}
-                    >
-                      Profile
-                    </Nav.Link>
-                    <Nav.Link
-                      onClick={() => {
-                        history.push("/plays");
-                      }}
-                    >
-                      Jugadas
-                    </Nav.Link>
-                    <Nav.Link
-                      onClick={() => {
-                        history.push("/pretemporada");
-                      }}
-                    >
-                      Pretemporada
-                    </Nav.Link>
-                    <NavDropdown.Divider />
-                    <Nav.Link onClick={logOutUser}>Logout</Nav.Link>
-                  </>
-                ) : (
-                  <>
-                    <Nav.Link
-                      onClick={() => {
-                        history.push("/signup");
-                      }}
-                    >
-                      Signup
-                    </Nav.Link>
-                    <Nav.Link
-                      onClick={() => {
-                        history.push("/login");
-                      }}
-                    >
-                      Login
-                    </Nav.Link>
-                  </>
-                )}
-                {/* <NavDropdown.Divider />
-                <Nav.Button onClick={logOutUser}>Logout</Nav.Button>*/}
-              </NavDropdown>
+            <Nav className="col-3">
+              <div className="right">
+                <NavDropdown
+                  title="Perfil"
+                  id="basic-nav-dropdown"
+                  className="desplegable"
+                >
+                  {isLoggedIn ? (
+                    <>
+                      <Nav.Link
+                        onClick={() => {
+                          history.push("/profile");
+                        }}
+                      >
+                        Profile
+                      </Nav.Link>
+                      <Nav.Link
+                        onClick={() => {
+                          history.push("/plays");
+                        }}
+                      >
+                        Jugadas
+                      </Nav.Link>
+                      <Nav.Link
+                        onClick={() => {
+                          history.push("/pretemporada");
+                        }}
+                      >
+                        Pretemporada
+                      </Nav.Link>
+                      <NavDropdown.Divider />
+                      <Nav.Link onClick={logOutUser}>Logout</Nav.Link>
+                    </>
+                  ) : (
+                    <>
+                      <Nav.Link
+                        onClick={() => {
+                          history.push("/signup");
+                        }}
+                      >
+                        Signup
+                      </Nav.Link>
+                      <Nav.Link
+                        onClick={() => {
+                          history.push("/login");
+                        }}
+                      >
+                        Login
+                      </Nav.Link>
+                    </>
+                  )}
+                </NavDropdown>
+              </div>
             </Nav>
           </Navbar.Collapse>
         </Container>
